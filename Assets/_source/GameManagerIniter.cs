e29373e8260a09a71c4aa2bbab09d1d9dbd72981
DevourDev <118769917+Pudge2005@users.noreply.github.com>
2023-04-03 13:59:17 +0300

    Pepega
    кликер с бафами и дебафами бафов, пока всё сырое

---
+++ Assets/_source/GameManagerIniter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    [DefaultExecutionOrder(-999)]
+    public sealed class GameManagerIniter : MonoBehaviour
+    {
+        [SerializeField] private GameManager _gameManager;
+        [SerializeField] private GameObject _networkGameManager;
+        [SerializeField, HideInInspector] private UnityEngine.Component _netGmComp;
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (_networkGameManager != null)
+            {
+                if (!_networkGameManager.TryGetComponent(typeof(INetworkGameManager), out var ngm))
+                {
+                    Debug.LogError($"Unable to find {nameof(INetworkGameManager)} component" +
+                        $" on {_networkGameManager.name} GameObject");
+
+                    _networkGameManager = null;
+                }
+                else
+                {
+                    _netGmComp = ngm;
+                }
+
+                UnityEditor.EditorUtility.SetDirty(this);
+            }
+        }
+#endif
+
+
+        private void Awake()
+        {
+            _gameManager.Init((INetworkGameManager)_netGmComp);
+        }
+    }
+}
---
+++ Assets/_source/Utils/SoDatabase.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Utils
+{
+    public abstract class SoDatabaseElement : ScriptableObject
+    {
+        [SerializeField, HideInInspector] private int _soDatabaseID;
+
+
+        public int GetDatabaseId() => _soDatabaseID;
+
+        internal void SetDatabaseId(int id) => _soDatabaseID = id;
+    }
+
+    public abstract class SoDatabase<TElement> : ScriptableObject
+        where TElement : SoDatabaseElement
+    {
+        [SerializeField] private List<TElement> _newElements;
+
+        [SerializeField] private List<TElement> _registeredElements;
+
+#if UNITY_EDITOR
+        private HashSet<TElement> _hs;
+#endif
+
+
+#if UNITY_EDITOR
+        private HashSet<TElement> HS
+        {
+            get
+            {
+                if (_hs == null)
+                    _hs = new();
+                else
+                    _hs.Clear();
+
+                return _hs;
+
+            }
+        }
+#endif
+
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (_newElements.Count > 0)
+            {
+                _registeredElements.AddRange(_newElements);
+                _newElements.Clear();
+                UnityEditor.EditorUtility.SetDirty(this);
+            }
+
+            CheckRegisteredElements();
+        }
+
+        private void CheckRegisteredElements()
+        {
+            var list = _registeredElements;
+            var count = list.Count;
+
+            bool modified = EnsureElementsUniqueness(in list, in count);
+
+            if (modified)
+                count = list.Count;
+
+            _ = ReAssignIds(in list, 0, in count);
+        }
+
+        private bool ReAssignIds(in List<TElement> list, int start, in int length)
+        {
+            bool reassigned = false;
+
+            for (int i = start; i < start + length; i++)
+            {
+                var el = list[i];
+
+                if (el.GetDatabaseId() == i)
+                    continue;
+
+                reassigned = true;
+                el.SetDatabaseId(i);
+                UnityEditor.EditorUtility.SetDirty(el);
+            }
+
+            return reassigned;
+        }
+
+        private bool EnsureElementsUniqueness(in List<TElement> list, in int count)
+        {
+            var hs = HS;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (list[i] != null)
+                    _ = hs.Add(list[i]);
+            }
+
+            bool modified = hs.Count != count;
+
+            if (modified)
+            {
+                list.Clear();
+                list.AddRange(hs);
+                UnityEditor.EditorUtility.SetDirty(this);
+            }
+
+            return modified;
+        }
+#endif
+        public TElement GetElement(int id)
+        {
+            return _registeredElements[id];
+        }
+    }
+}
